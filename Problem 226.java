4ed15c34f9046d13a75f64d6226136ec033f77e3
Mohit Tiwari <mohittiwari17091709@gmail.com>
2025-08-25 22:28:58 +0530

    Update Problem 226.java

--- Problem 226.java
+++ Problem 226.java
@@ -1,1 +1,12 @@
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null)
+      return null;
 
+    TreeNode left = root.left;
+    TreeNode right = root.right;
+    root.left = invertTree(right);
+    root.right = invertTree(left);
+    return root;
+  }
+}
